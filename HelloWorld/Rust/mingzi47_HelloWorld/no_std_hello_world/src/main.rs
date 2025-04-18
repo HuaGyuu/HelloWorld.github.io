497c91be51c45b0cc6290d8901beafdb3be6c08b
mingzi <1409429504@qq.com>
2025-04-18 20:12:55 +0800

    feat: use ASCII "Hello World"

--- HelloWorld/Rust/mingzi47_HelloWorld/no_std_hello_world/src/main.rs
+++ HelloWorld/Rust/mingzi47_HelloWorld/no_std_hello_world/src/main.rs
@@ -5,18 +5,30 @@ const UART0: *mut u8 = 0x1000_0000 as *mut u8;
 const EXIT_ADDR: u64 = 0x100000;
 const EXIT_CODE: u64 = 0x5555;
 
-static HELLO: &[u8] = b"\x1b[31mHello World!\x1b[0m\n";
+static HELLO: &[&[u8]] = &[
+    b"\n",
+    // red
+    b"\x1b[31m",
+    // Hello World
+    b"88  88 888888 88     88      dPYb Yb        dP dPYb  88\"\"Yb 88     8888b\n",
+    b"88  88 88__   88     88     dP  Yb Yb  db  dP dP  Yb 88__dP 88     8I  Yb\n",
+    b"888888 88\"\"   88  .o 88  .o Yb  dP  YbdPYbdP  Yb  dP 88\"Yb  88  .o 8I  dY\n",
+    b"88  88 888888 88ood8 88ood8  YboP    YP  YP    YboP  88  Yb 88ood8 8888Y\"\n",
+    // color end
+    b"\x1b[0m",
+];
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    HELLO
-        .iter()
-        .for_each(|&c| {
-            unsafe {
-                // output
-                UART0.write_volatile(c);
-            }
-        });
+    for line in HELLO {
+        line.iter()
+            .for_each(|&c| {
+                unsafe {
+                    // output
+                    UART0.write_volatile(c);
+                }
+            });
+    }
 
     // exit qemu
     unsafe {
